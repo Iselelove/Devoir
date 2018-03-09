aa54f508135dd2169ac809a79ffcbec1060400d6
Iselelove <37224025+Iselelove@users.noreply.github.com>
2018-03-09 22:07:14 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RATTRAPAGE1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maison maison1 = new Maison("btmt25", 5);
+            Batiment batiment1 = new Batiment("btmt150");
+
+        }
+    }
+}
